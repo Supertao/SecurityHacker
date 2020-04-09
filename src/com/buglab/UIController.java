530905ba821e7f3a94752141fe2c8adf3846e62b
Buglab <taochao@huawei.com>
2020-04-09 10:01:33 +0800

    修改下按钮的绑定事件

--- src/com/buglab/UIController.java
+++ src/com/buglab/UIController.java
@@ -11,23 +11,27 @@ import java.io.File;
 //https://www.w3cschool.cn/java/javafx-filechooser.html
 
 public class UIController {
-    @FXML
-    private Button codeql_excute_bt,codeql_database_bt,codeql_source_bt;
+   
     private FileChooser fileChooser;
     private Stage fileStage=null;
 
     @FXML
-    public void codeql_excute_click()
-    {
-        codeql_excute_bt=new Button();
-        //构建一个文件选择器
-        fileChooser=new FileChooser();
-        fileChooser.setTitle("Select CodeQl ExcutePath:");
-        File selectedFile=fileChooser.showOpenDialog(fileStage);
-        String path=selectedFile.getPath();
-        System.out.println(path);
-        codeql_excute_bt.setLabel(path);
+    private Button codeql_excute_button;
 
+    @FXML
+    private  void initialize(){
+
+        codeql_excute_button.setOnAction(event -> {
+            //构建一个文件选择器
+            fileChooser=new FileChooser();
+            fileChooser.setTitle("Select CodeQl ExcutePath:");
+            File selectedFile=fileChooser.showOpenDialog(fileStage);
+            String path=selectedFile.getPath();
+            if(path!=""){
+                codeql_excute_button.setText(path);
+            }
+
+        });
     }
 
     @FXML
